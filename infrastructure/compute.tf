623f06300d02582bba71af9a16326e4da7346a5c
jameswachira394-lab <jameswachira394@gmail.com>
2026-06-13 13:06:31 +0300

    compute

--- infrastructure/compute.tf
+++ infrastructure/compute.tf
@@ -1,7 +1,8 @@
-# ---------------- Ubuntu EC2 ----------------
 resource "aws_instance" "ubuntu" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = var.instance_type
+
+  key_name = "forex-mt5-key"
 
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ubuntu_sg.id]
@@ -9,6 +10,7 @@ resource "aws_instance" "ubuntu" {
   iam_instance_profile = aws_iam_instance_profile.profile.name
 
   associate_public_ip_address = true
+
 
   user_data = <<-EOF
               #!/bin/bash
@@ -41,16 +43,17 @@ data "aws_ami" "windows" {
 }
 
 # ---------------- Windows EC2 ----------------
+
 resource "aws_instance" "windows" {
   ami           = data.aws_ami.windows.id
   instance_type = var.instance_type
+
+  key_name = "forex-mt5-key"
 
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.windows_sg.id]
 
   iam_instance_profile = aws_iam_instance_profile.profile.name
-
-  associate_public_ip_address = true
 
   tags = {
     Name = "windows-ec2"
